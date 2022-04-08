ede910aa9762cf47522f6647975d954c92564d12
Elisabeth Rousset <lisbeth.rousset@gmail.com>
2022-04-08 15:46:55 +0200

    added rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,50 @@
-// import de nos plugins
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from 'rollup-plugin-terser';
+
+const MODULE_NAME = 'THREECapsuleBufferGeometry';
+const MODULE_FILENAME = 'three-js-capsule-geometry';
+const DIST = './dist';
+
+// external libs which must not be bundled
+const externals = [
+    'three'
+];
+
+// globals where the external libs are expected (iife only)
+const globals = {
+    'three': 'THREE'
+}
 
 export default {
     input: './src/exports.js',
-    output: {
-        file: './dist/browser/three-js-capsule-geometry.js',
-        format: 'cjs'
-    },
-    external: ['three-full'],
     plugins: [
-        commonjs(), // prise en charge de require
-        resolve(), // prise en charge des modules depuis node_modules
+        commonjs(), // handles requires in CJS dependancies
+        resolve(), // resolves node_module dependancies
+    ],
+    external: externals,
+    output: [
+        // output options
+
+        {   // for bundlers
+            format: 'esm',
+            file: `${DIST}/${MODULE_FILENAME}.mjs`,
+        },
+        {   // for browser (debug)
+            format: 'iife',
+            name: MODULE_NAME,
+            globals: globals,
+            file: `${DIST}/${MODULE_FILENAME}.js`,
+            sourcemap: true, // for easier debugging in dev tools
+        },
+        {   // for browser (minified)
+            format: 'iife',
+            name: MODULE_NAME,
+            globals: globals,
+            file: `${DIST}/${MODULE_FILENAME}.min.js`,
+            plugins: [
+                terser(), // minify
+            ]
+        },
     ]
 };
